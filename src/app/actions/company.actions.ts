a563342c1bec3c23a8a0f2e1895113a8fb7e128f
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-10-05 16:26:43 +0200

    Add company action file

---
+++ src/app/actions/company.actions.ts
@@ -0,0 +1,10 @@
+export const LOAD_COMPANIES = 'LOAD_COMPANIES';
+export const LOAD_COMPANIES_SUCCESS = 'LOAD_COMPANIES_SUCCESS';
+
+export class LoadCompaniesAction {
+  readonly type = LOAD_COMPANIES;
+
+  constructor() {
+    // empty
+  }
+}
