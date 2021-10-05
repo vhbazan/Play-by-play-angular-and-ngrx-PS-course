add5f2dc0fbf2bfd5892b24afea86511e337363b
Victor Hugo Bazan <vhbazanm@gmail.com>
2021-10-05 16:31:38 +0200

    Add another action and create Action type

--- src/app/actions/company.actions.ts
+++ src/app/actions/company.actions.ts
@@ -1,3 +1,5 @@
+import { Company } from "app/company/company";
+
 export const LOAD_COMPANIES = 'LOAD_COMPANIES';
 export const LOAD_COMPANIES_SUCCESS = 'LOAD_COMPANIES_SUCCESS';
 
@@ -8,3 +10,14 @@ export class LoadCompaniesAction {
     // empty
   }
 }
+
+export class LoadCompaniesSuccessAction {
+  readonly type = LOAD_COMPANIES_SUCCESS;
+
+  constructor(public payload: Company[]) {
+    // empty
+  }
+}
+
+export type Action =
+  LoadCompaniesAction | LoadCompaniesSuccessAction;
